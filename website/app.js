afba6e0d2b14a25dcd60eed2e75f4f67bcbb7ce0
cavilosa <cavilosa33@gmail.com>
2020-12-14 10:33:07 -0700

    API call workes

--- website/app.js
+++ website/app.js
@@ -9,25 +9,37 @@ function handleSubmit(event) {
     console.log("::: Form Submitted :::")
 
     getAPI('http://localhost:8000/test')
-    /*.then ( (url) => {
-        getData(url);
-    })*/
+    .then ( (apiKey) => {
+        getURL(apiKey)
+        .then(  (url) => {
+                getData(url)
+                .then( (data) => {
+                    const results = `The irony of the text: ${data.irony}.
+                    The subjectivity of the text: ${data.subjectivity}.
+                    The agreement between the sentiments detected in the text,
+                    the sentence or the segment it refers to: ${data.agreement}.`
+                    document.querySelector("#results").innerText = results;
+                })
+        } )
+    })
 
 }
 
+
 const getData = async (url) => {
+    console.log("url", url);
     const response = await fetch(url);
     if (response.status > 300) {
         window.alert("response status is > 300");
     }
     try {
         const data = await response.json();
+        console.log("data", data);
         return data;
     }catch (error) {
         console.log("error", error);
     }
 }
-
 
 const getAPI = async (url) => {
     console.log(url);
@@ -37,7 +49,7 @@ const getAPI = async (url) => {
     }
     try {
         const apiKey = response.json();
-        console.log("key", apiKey);
+        //console.log("key:", apiKey);
         return apiKey;
         //getURL(apiKey);
     }catch(error) {
@@ -46,6 +58,7 @@ const getAPI = async (url) => {
 }
 
 const getURL = async (apiKey) => {
+    console.log(apiKey);
     const text = document.getElementById('name').value;
     if (text === "") {
         alert("Fill in the text field");
