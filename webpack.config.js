bc970f405d2951af0e47d70f6b62ce4fc23a4c0a
cavilosa <cavilosa33@gmail.com>
2020-12-14 12:03:22 -0700

    Hardcorded reference to js file in dist folder into HTML, added bableloader into config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,17 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                presets: ['@babel/preset-env']
+                }
+            }
+        }
+    }
 };
