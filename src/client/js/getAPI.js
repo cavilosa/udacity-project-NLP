e2e896497644838aa6d6efcebfbe0ef94826658b
cavilosa <cavilosa33@gmail.com>
2020-12-16 20:10:19 -0700

    response changed to request

--- src/client/js/getAPI.js
+++ src/client/js/getAPI.js
@@ -1,19 +1,12 @@
 async function getAPI (url) {
-    console.log(url);
 
-    const response = await fetch(`${url}`,{
-      headers : {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-       method: "GET"
-    })
-    console.log(response);
-    if (response.status != 200) {
+    const request = await fetch(`${url}`)
+
+    if (request.status != 200) {
         console.log(response.status)
     }
     try {
-        const apiKey = response.json()
+        const apiKey = request.json()
         console.log(apiKey)
         return apiKey;
     } catch(error) {
