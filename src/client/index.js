a76606dc632063379435d19ba60e35a21a379d3b
cavilosa <cavilosa33@gmail.com>
2020-12-17 14:11:27 -0700

    deleted unneccesary js files

--- src/client/index.js
+++ src/client/index.js
@@ -1,16 +1,12 @@
 import { handleSubmit } from "./js/app.js";
 import { postText } from "./js/getAPI.js";
-import { getURL } from "./js/getURL.js";
 import { updateUI } from "./js/getData.js";
 
 
 import "./styles/style.scss";
 
-console.log("index.js is running");
-
 export {
     handleSubmit,
     postText,
-    getURL,
     updateUI
 }
--- src/client/js/getData.js
+++ src/client/js/getData.js
@@ -1,21 +1,3 @@
-async function getData(url) {
-    console.log("url", url);
-    const response = await fetch(url);
-    if (response.status > 300) {
-        window.alert("response status is > 300");
-    }
-    try {
-        const data = await response.json();
-        console.log("data", data);
-        return data;
-    }catch (error) {
-        console.log("error", error);
-    }
-}
-
-export { getData }
-
-
 async function updateUI (url) {
     console.log("update running")
     const request = await fetch (url);
